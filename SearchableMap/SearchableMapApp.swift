9b92f78f7d81eeb701d545c62d3247ce7fc1f15a
Nathan Ulrich <nateulrich@gmail.com>
2023-10-25 15:14:44 -0400

    Initial Commit

---
+++ SearchableMap/SearchableMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableMapApp.swift
+//  SearchableMap
+//
+//  Created by Nathan Ulrich on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SearchableMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
